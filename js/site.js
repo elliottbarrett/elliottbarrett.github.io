af73574df452d9d7cbb03978a03d4fa0dcd6b0ba
Elliott Barrett <elliottjbarrett@gmail.com>
2019-12-02 11:02:53 -0400

    Added other html files, updated JS to better reflect the program running

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 var camera, scene, renderer;
 
-const NUM_SNOWFLAKES = 200;
-const snowflakes = [];
+const NUM_QUADS = 250;
+const quads = [];
 
 function init() {
   camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
@@ -9,21 +9,21 @@ function init() {
 
   scene = new THREE.Scene();
 
-  for (let i=0; i < NUM_SNOWFLAKES; i++) {
-    // const snowflakeGeometry = new THREE.PlaneGeometry(0.1, 0.1);
-    const snowflakeGeometry = new THREE.PlaneGeometry(0.08, 0.08);
-    const snowflakeMaterial = new THREE.MeshBasicMaterial({
-      color: new THREE.Color(i/NUM_SNOWFLAKES, 0.4, 1 - i/NUM_SNOWFLAKES),
+  for (let i=0; i < NUM_QUADS; i++) {
+    // const quadGeometry = new THREE.PlaneGeometry(0.1, 0.1);
+    const quadGeometry = new THREE.PlaneGeometry(0.08, 0.08);
+    const quadMaterial = new THREE.MeshBasicMaterial({
+      color: new THREE.Color(i/NUM_QUADS, 0.4, 1 - i/NUM_QUADS),
       side: THREE.DoubleSide
     });
-    const snowflakePlane = new THREE.Mesh(snowflakeGeometry, snowflakeMaterial);
-    snowflakePlane.position.x = 3 * i/NUM_SNOWFLAKES - 1.5;
-    // snowflakePlane.position.z = i/NUM_SNOWFLAKES - 1;
-    snowflakePlane.rotation.x = i/NUM_SNOWFLAKES;
-    snowflakePlane.rotation.y = i/NUM_SNOWFLAKES;
-    snowflakePlane.rotation.z = i/NUM_SNOWFLAKES;
-    snowflakes.push(snowflakePlane);
-    scene.add(snowflakePlane);
+    const quadPlane = new THREE.Mesh(quadGeometry, quadMaterial);
+    quadPlane.position.x = 3 * i/NUM_QUADS - 1.5;
+    // quadPlane.position.z = i/NUM_QUADS - 1;
+    quadPlane.rotation.x = i/NUM_QUADS;
+    quadPlane.rotation.y = i/NUM_QUADS;
+    quadPlane.rotation.z = i/NUM_QUADS;
+    quads.push(quadPlane);
+    scene.add(quadPlane);
   }
 
   renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -34,17 +34,18 @@ function init() {
 function animate() {
   requestAnimationFrame( animate );
 
-  snowflakes.forEach((flake, index) => {
+  quads.forEach((quad, index) => {
     const t = new Date().getTime();
-    flake.rotation.x += 0.01;
-    flake.rotation.y += 0.02;
-    flake.position.y = 0.5 * Math.sin(((index + 1)/NUM_SNOWFLAKES * 2 * Math.PI * t/3000))
-    let color = new THREE.Color((index + 1)/NUM_SNOWFLAKES, 0.4, index/NUM_SNOWFLAKES);
-    // color = color.multiplyScalar(0.5 - 0.5 * Math.sin((index/NUM_SNOWFLAKES * 2 * Math.PI * t/1000)))
-    const r = 0.5 - 0.5 * Math.sin(((index + 1)/NUM_SNOWFLAKES * 2 * Math.PI * t/1000));
-    const g = 0.5 - 0.5 * Math.cos(((index + 1)/NUM_SNOWFLAKES * 2 * Math.PI * t/1000))
+    quad.rotation.x += 0.01;
+    quad.rotation.y += 0.02;
+    quad.position.y = 0.5 * Math.sin(((index + 1)/NUM_QUADS * 2 * Math.PI * t/3000))
+    // quad.position.z = -1
+    let color = new THREE.Color((index + 1)/NUM_QUADS, 0.4, index/NUM_QUADS);
+    // color = color.multiplyScalar(0.5 - 0.5 * Math.sin((index/NUM_QUADS * 2 * Math.PI * t/1000)))
+    const r = 0.5 - 0.5 * Math.sin(((index + 1)/NUM_QUADS * 2 * Math.PI * t/1000));
+    const g = 0.5 - 0.5 * Math.cos(((index + 1)/NUM_QUADS * 2 * Math.PI * t/1000))
     color = new THREE.Color(r, g, 0.6);
-    flake.material.color = color;
+    quad.material.color = color;
   })
 
   renderer.render( scene, camera );
